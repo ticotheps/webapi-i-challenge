1664ba1330afd5ffb416cc4cb65415a014d9b735
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-18 15:51:34 -0400

    Creates a server that runs on port 4000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+server.use(express.json());
+
+server.listen(4000, () => {
+    console.log('API up and running on port 4000');
+});
