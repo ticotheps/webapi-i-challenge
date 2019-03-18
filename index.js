85b9a3a38132035c25268ebde1279b773785e185
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-18 16:31:08 -0400

    Creates a value for each key in the module.exports object inside of db.js file and also creates a GET request to return 'users' array from endpoint

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ server.get('/', (req, res) => {
     res.send('The Express server for the Users Project is LIVE!');
 });
 
+// Returns an array of all the user objects contained in the database
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            // 200-299 = success
+            // 300-399 = rediret
+            // 400-499 = client error
+            // 500-599 = server error
+            res.status(200).json(users);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'error returning users' });
+        });
+});
+
 server.listen(4000, () => {
     console.log('API up and running on port 4000');
 });
