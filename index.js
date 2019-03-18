09f70eb88441e81e7d4cf22dd52455773de47856
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-18 16:35:36 -0400

    Creates a GET request to return the user of a specified id

--- index.js
+++ index.js
@@ -25,6 +25,22 @@ server.get('/api/users', (req, res) => {
         });
 });
 
+// Returns the user object with the specified id
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    db.findById(id)
+        .then(user => {
+            // 200-299 = success
+            // 300-399 = rediret
+            // 400-499 = client error
+            // 500-599 = server error
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'error returning user with matching id' });
+        });
+});
+
 server.listen(4000, () => {
     console.log('API up and running on port 4000');
 });
