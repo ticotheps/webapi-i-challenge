733aad9c1c7461523564f06c94be47e54907230f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-18 15:55:46 -0400

    Creates a GET request to display message on server's landing page

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const db = require('./data/db.js');
 const server = express();
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.send('The Express server for the Users Project is LIVE!');
+});
+
 server.listen(4000, () => {
     console.log('API up and running on port 4000');
 });
